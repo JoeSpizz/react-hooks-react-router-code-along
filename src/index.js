d6ccedea2832e19c7892d238f81c72e009c34f0d
Joe <spizjo01@gmail.com>
2022-08-31 13:30:38 -0600

    commit tree 763d50786003ea3408659cc82aa7deef2ee3b59f

--- src/index.js
+++ src/index.js
@@ -1,6 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Switch } from "react-router-dom";
+import {BrowserRouter, Route, NavLink} from "react-router-dom"
 
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "green",
+  textDecoration: "none",
+  color: "white",
+};
+function NavBar() {
+  return (
+    <div>
+      <NavLink
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add styling to Navlink */
+        style={linkStyles}
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/signup"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Sign-Up
+      </NavLink>
+      <NavLink
+        to="/messages"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Messages
+      </NavLink>
+    </div>
+  );
+}
 function Home() {
   return (
     <div>
@@ -8,9 +78,74 @@ function Home() {
     </div>
   );
 }
-
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+function Messages() {
+  return (
+    <div>
+      <h3>Prepare to read some messages!</h3>
+      <p>MESSAGES NOT FOUND</p>
+    </div>
+  );
+}
+
+function Signup() {
+  return (
+    <div>
+      <h3>Prepare to read some messages!</h3>
+      <p>MESSAGES NOT FOUND</p>
+    </div>
+   
+  );
+}
+function App() {
+  return (
+  <div>
+    <NavBar/>
+    <Switch>
+    <Route path="/about">
+    <About />
+    </Route>;
+    <Route path="/login">
+    <Login />
+    </Route>
+    <Route exact path="/">
+    <Home />
+    </Route>
+    <Route exact path="/signup">
+    <Signup />
+    </Route>
+    <Route exact path="/messages">
+    <Messages />
+    </Route>
+    </Switch>
+    </div>)
+}
+
+ReactDOM.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>, document.getElementById("root"));
